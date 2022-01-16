8d22b72bd499792f472a1ca801cdcca48897df58
João Paulo Januario <joaopaulo@Joaos-MacBook-Pro.local>
2022-01-16 13:51:09 -0300

    Types page layout

--- lib/presentation/pages/pokemon_types_page.dart
+++ lib/presentation/pages/pokemon_types_page.dart
@@ -1,6 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokedex/constants/app_colors.dart';
+import 'package:pokedex/constants/app_dimens.dart';
+import 'package:pokedex/constants/app_strings.dart';
 import 'package:pokedex/domain/entities/ui_state.dart';
 import 'package:pokedex/presentation/cubits/pokemon_types_cubit.dart';
 import 'package:pokedex/presentation/widgets/failure_widget.dart';
@@ -34,7 +38,9 @@ class _PageContentState extends State<_PageContent> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text(AppStrings.appName),
+      ),
       body: BlocBuilder<PokemonTypesCubit, PokemonTypesState>(
         bloc: _cubit,
         buildWhen: (prev, curr) => prev.uiState != curr.uiState,
@@ -46,12 +52,54 @@ class _PageContentState extends State<_PageContent> {
           } else if (state.uiState == UIState.success) {
             return ListView.builder(
               itemCount: state.pokemonTypes.length,
+              padding: const EdgeInsets.fromLTRB(
+                kMarginDefault,
+                kMarginDefault,
+                kMarginDefault,
+                kMarginBig,
+              ),
               itemBuilder: (context, index) {
                 final type = state.pokemonTypes[index];
-                return ListTile(
-                  title: Text(type.name),
-                  subtitle: Text(type.url),
-                  onTap: () => _cubit.onPokemonTypeSelected(type),
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: InkWell(
+                    onTap: () => _cubit.onPokemonTypeSelected(type),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          color: AppColors.typeColor(type),
+                          height: 100.0,
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: AutoSizeText(
+                              type.name,
+                              style: TextStyle(
+                                color: Colors.black.withOpacity(0.04),
+                                fontSize: 180.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          type.name,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 );
               },
             );
